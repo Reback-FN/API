ab6ff64c3f38d4f748070c1a9352f81346ea699f
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-10 18:15:50 +0100

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,2 +1,11 @@
-npm run start
+@echo off
+
+IF NOT EXIST "node_modules" (
+    echo The "node_modules" folder does not exist. Installing dependencies..
+    npm install
+) ELSE (
+    echo The "node_modules" folder already exists. Starting the server..
+    npm run start
+)
+
 pause
