8e229240dc1cf255a181b61fcac3854616dcb0c3
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-10 18:12:50 +0100

    Add files via upload

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
 const ApiRouter = Router();
 
-const V1 = {
-    API: require('./v1/api.js'),
-    EOS: require('./v1/eos.js'),
-    STORE: require('./v1/store.js')
+const V2 = {
+    API: require('./v2/api.js'),
+    EOS: require('./v2/eos.js'),
+    STORE: require('./v2/store.js'),
+    OAUTH: require('./v2/oauth.js')
 }
 
-ApiRouter.use('/api', V1.API);
-ApiRouter.use('/eos', V1.EOS);
-ApiRouter.use('/store', V1.STORE);
+ApiRouter.use('/api', V2.API);
+ApiRouter.use('/eos', V2.EOS);
+ApiRouter.use('/store', V2.STORE);
 
 module.exports = ApiRouter;
